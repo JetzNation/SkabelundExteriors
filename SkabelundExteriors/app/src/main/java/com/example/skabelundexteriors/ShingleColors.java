990f786f576a4e7178bfc6b68e88e73dce749e74
andrewskabelund <andyskabelund2@gmail.com>
2022-05-07 13:32:25 -0600

    Shingle pic and updated nav graph.

--- SkabelundExteriors/app/src/main/java/com/example/skabelundexteriors/ShingleColors.java
+++ SkabelundExteriors/app/src/main/java/com/example/skabelundexteriors/ShingleColors.java
@@ -3,62 +3,30 @@ package com.example.skabelundexteriors;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ShingleColors#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ShingleColors extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ShingleColors() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ShingleColors.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ShingleColors newInstance(String param1, String param2) {
-        ShingleColors fragment = new ShingleColors();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shingle_colors, container, false);
+        NavController navController = NavHostFragment.findNavController(this);
+
+        View view = inflater.inflate(R.layout.fragment_shingle_colors, container, false);
+
+        view.findViewById(R.id.black_walnut).setOnClickListener(button -> {
+            navController.navigate(R.id.action_shingleColors_to_blackWalnut);
+        });
+        view.findViewById(R.id.forest_green).setOnClickListener(button -> {
+            navController.navigate(R.id.action_shingleColors_to_forestGreen);
+        });
+
+        return view;
     }
 }
--- SkabelundExteriors/app/src/main/java/com/example/skabelundexteriors/forestGreen.java
+++ SkabelundExteriors/app/src/main/java/com/example/skabelundexteriors/forestGreen.java
@@ -3,21 +3,19 @@ package com.example.skabelundexteriors;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class BlackWalnut extends Fragment {
+
+public class forestGreen extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_black_walnut, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_forest_green, container, false);
         return view;
     }
 }
